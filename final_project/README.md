7374b0a3c20af302597bae346bf498ce925db253
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-02-23 22:34:45 +0330

    dependencies

--- final_project/README.md
+++ final_project/README.md
@@ -1,1 +1,3 @@
 Practice-Project
+
+Developing an online book review application using Node.js and Express.js
--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Check if the user is authenticated using the access token
+if (req.session.accessToken) {
+    // User is authenticated, proceed to the next middleware or route
+    next();
+  } else {
+    // User is not authenticated, redirect to login or send an error response
+    res.status(401).json({ error: "Unauthorized" });
+  }
 });
  
 const PORT =5000;
@@ -20,4 +27,3 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
- 
